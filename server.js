35b58b05418cca366bd112f772931fa8fb3fb745
saraisa12 <saraisa.024@gmail.com>
2024-10-19 17:48:17 +0300

    authentication done

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose")
 const express = require("express")
+const logger = require("morgan")
+const cors = require("cors")
 
 // Load environment variables from .env file
 require("dotenv").config()
@@ -12,9 +14,17 @@ const PORT = process.env.PORT || 4000
 
 const app = express()
 
+app.use(cors())
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 // Import routes
 
+const AuthRouter = require("./routes/authRouter")
+
 //mount routes
+app.use("/auth", AuthRouter)
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
