25f18800c6aea8613306d0aa116b1b91bcc09d36
FatemaJadeed <fatema.jadeed266@gmail.com>
2024-10-23 18:29:25 +0300

    update

--- routes/trip.js
+++ routes/trip.js
@@ -1,32 +1,32 @@
-const express = require("express")
-const { stripToken, verifyToken } = require("../middleware/index")
+const express = require('express')
+const { stripToken, verifyToken } = require('../middleware/index')
 const router = express.Router()
 
 router.use(express.urlencoded({ extended: true }))
 
 // Import controller
-const upload = require("../middleware/upload")
-const tripCntrl = require("../controllers/trip")
+const upload = require('../middleware/upload')
+const tripCntrl = require('../controllers/trip')
 
 // Routes
 router.post(
-  "/add",
+  '/add',
   stripToken,
   verifyToken,
 
-  upload.single("image"),
+  upload.single('image'),
   tripCntrl.trip_create_post
 )
 
 router.delete(
-  "/delete/:id",
+  '/delete/:id',
   stripToken,
   verifyToken,
   tripCntrl.trip_delete_delete
 )
 
-router.get("/index", stripToken, verifyToken, tripCntrl.get_user_trips)
+router.get('/index', stripToken, verifyToken, tripCntrl.get_user_trips)
 
-router.get("/details/:id", stripToken, verifyToken, tripCntrl.trip_details_get)
+router.get('/details/:id', stripToken, verifyToken, tripCntrl.trip_details_get)
 
 module.exports = router
